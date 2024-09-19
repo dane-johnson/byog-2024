62aac6dbae850039e79d0b647d3a2379f5c1e7ea
Dane Johnson <daneallenjohnson@protonmail.com>
2024-09-19 16:37:50 -0500

    Animations

--- godot/controllers/character_controller.gd
+++ godot/controllers/character_controller.gd
@@ -10,6 +10,14 @@ var camera_rig: Node3D = get_node_or_null(camera_rig_path)
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity_vector") \
 		* ProjectSettings.get_setting("physics/3d/default_gravity")
+		
+func _process(_delta: float) -> void:
+	if velocity != Vector3.ZERO:
+		$Slime.move()
+		$Slime.look_at(position + velocity) ## TODO interpolate
+	else:
+		$Slime.idle()
+	
 
 func _physics_process(delta: float) -> void:
 	var inputs = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
