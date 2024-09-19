2975bc4bf92343cfce3aac456cad238b48cd4ed7
Dane Johnson <daneallenjohnson@protonmail.com>
2024-09-19 17:41:03 -0500

    Animate player effects, pull back to see more of the level

--- godot/characters/slime.gd
+++ godot/characters/slime.gd
@@ -1,8 +1,5 @@
 extends Node3D
 
-func _process(delta: float) -> void:
-	$Skeleton3D/Cube.get_surface_override_material(0)["albedo_texture"].noise.offset.y += delta * 10
-
 func move() -> void:
 	$AnimationTree["parameters/Blend/blend_amount"] = 0
 func idle() -> void:
