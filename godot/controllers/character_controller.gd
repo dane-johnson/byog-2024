40d472b80956dbd44f8c4841221e761c47950b8f
Dane Johnson <daneallenjohnson@protonmail.com>
2024-09-19 15:54:12 -0500

    Fix movement script

--- godot/controllers/character_controller.gd
+++ godot/controllers/character_controller.gd
@@ -14,12 +14,13 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity_vector") \
 func _physics_process(delta: float) -> void:
 	var inputs = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	
-	velocity += get_camera_direction() * Vector3(inputs.x, 0.0, inputs.y) * speed * delta
+	velocity *= Vector3.UP ## Kill horizontal movement
+	velocity += get_camera_direction() * Vector3(inputs.x, 0.0, inputs.y) * speed
 	velocity += gravity * delta
 	move_and_slide()
 
 func get_camera_direction() -> Basis:
 	if camera_rig:
-		return camera_rig.basis
+		return Basis.from_euler(Vector3.UP * camera_rig.rotation.y)
 	else:
 		return Basis.IDENTITY
